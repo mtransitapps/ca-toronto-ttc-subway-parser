099a1dcb2556ff3b827f3b73657150a1ef55ae87
Mathieu Méa <mathieu.mea@gmail.com>
2018-05-05 11:12:57 -0400

    Compatibility with latest update

--- src/org/mtransit/parser/ca_toronto_ttc_subway/TorontoTTCSubwayAgencyTools.java
+++ src/org/mtransit/parser/ca_toronto_ttc_subway/TorontoTTCSubwayAgencyTools.java
@@ -147,7 +147,7 @@ public class TorontoTTCSubwayAgencyTools extends DefaultAgencyTools {
 	private static HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
 	static {
 		HashMap<Long, RouteTripSpec> map2 = new HashMap<Long, RouteTripSpec>();
-		map2.put(1l, new RouteTripSpec(1l, //
+		map2.put(1L, new RouteTripSpec(1L, //
 				0, MTrip.HEADSIGN_TYPE_STRING, DOWNSVIEW, //
 				1, MTrip.HEADSIGN_TYPE_STRING, FINCH) //
 				.addTripSort(0, //
@@ -182,6 +182,7 @@ public class TorontoTTCSubwayAgencyTools extends DefaultAgencyTools {
 								"14457", // BLOOR STATION - NORTHBOUND PLATFORM
 								"14460", // == ST CLAIR STATION - NORTHBOUND PLATFORM
 								"14461", // == DAVISVILLE STATION - NORTHBOUND PLATFORM
+								"14462", // EGLINTON STATION - NORTHBOUND PLATFORM
 								"14463", // == LAWRENCE STATION - NORTHBOUND PLATFORM
 								"14464", // != YORK MILLS STATION - NORTHBOUND PLATFORM
 								"14467", // == FINCH STATION - SUBWAY PLATFORM
@@ -227,7 +228,7 @@ public class TorontoTTCSubwayAgencyTools extends DefaultAgencyTools {
 	@Override
 	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
 		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
 		}
 		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
 	}
@@ -243,7 +244,7 @@ public class TorontoTTCSubwayAgencyTools extends DefaultAgencyTools {
 	@Override
 	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
 		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
 		}
 		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
 	}
